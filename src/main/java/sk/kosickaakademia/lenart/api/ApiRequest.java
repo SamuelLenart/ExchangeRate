cf165dd09d2dc4bf124109a0c637b81bb03bacba
Samuel Lenart <samuel.lenart@kosickaakademia.sk>
2021-04-08 10:34:27 +0200

    add ExchangeRate, ApiRequest

---
+++ src/main/java/sk/kosickaakademia/lenart/api/ApiRequest.java
@@ -0,0 +1,16 @@
+package sk.kosickaakademia.lenart.api;
+
+import java.util.Map;
+import java.util.Set;
+
+public class ApiRequest {
+    public Map getExchangeRate(Set<String> rate){
+        if(rate==null || rate.size()==0)
+            return null;
+        return null;
+    }
+
+    private void getRatesFromAPIServer(){
+
+    }
+}
