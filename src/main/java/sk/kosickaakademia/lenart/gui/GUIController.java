aa4a030e88089ecfd3d431d4c0caab58284b8b09
Samuel Lenart <samuel.lenart@kosickaakademia.sk>
2021-04-12 09:24:05 +0200

    Add Gui

---
+++ src/main/java/sk/kosickaakademia/lenart/gui/GUIController.java
@@ -0,0 +1,13 @@
+package sk.kosickaakademia.lenart.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUIController {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    public void start(Stage primaryStage){
+
+    }
+}
